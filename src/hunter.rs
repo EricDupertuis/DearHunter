6828e755e753e5ba47f01dce950e6b94cbefbeb3
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2019-01-25 21:41:34 +0100

    Render hunter as white box sprite

---
+++ src/hunter.rs
@@ -0,0 +1,120 @@
+use amethyst::{
+    assets::{AssetStorage, Loader},
+    core::transform::Transform,
+    ecs::prelude::{Component, DenseVecStorage},
+    prelude::*,
+    renderer::{
+        Camera, PngFormat, Projection, SpriteRender, SpriteSheet, SpriteSheetFormat,
+        SpriteSheetHandle, Texture, TextureMetadata,
+    },
+    utils::application_root_dir,
+};
+
+const ARENA_HEIGHT: f32 = 100.0;
+const ARENA_WIDTH: f32 = 100.0;
+
+const PADDLE_HEIGHT: f32 = 16.0;
+const PADDLE_WIDTH: f32 = 4.0;
+
+pub struct Hunter;
+
+impl SimpleState for Hunter {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        // Load the spritesheet necessary to render the graphics.
+        // `spritesheet` is the layout of the sprites on the image;
+        // `texture` is the pixel data.
+        let sprite_sheet_handle = load_sprite_sheet(world);
+
+        world.register::<Paddle>();
+
+        initialise_hunter(world, sprite_sheet_handle);
+        initialise_camera(world);
+    }
+}
+
+struct Paddle {
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Paddle {
+    fn new() -> Paddle {
+        Paddle {
+            width: PADDLE_WIDTH,
+            height: PADDLE_HEIGHT,
+        }
+    }
+}
+
+impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+fn load_sprite_sheet(world: &mut World) -> SpriteSheetHandle {
+    // Load the sprite sheet necessary to render the graphics.
+    // The texture is the pixel data
+    // `sprite_sheet` is the layout of the sprites on the image
+    // `texture_handle` is a cloneable reference to the texture
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            format!("{}/resources/sprites/hunter.png", application_root_dir()),
+            PngFormat,
+            TextureMetadata::srgb_scale(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        format!("{}/resources/sprites/hunter.ron", application_root_dir()), // Here we load the associated ron file
+        SpriteSheetFormat,
+        texture_handle, // We pass it the texture we want it to use
+        (),
+        &sprite_sheet_store,
+    )
+}
+
+/// Initialise the camera.
+fn initialise_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_xyz(0.0, 0.0, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::from(Projection::orthographic(
+            0.0,
+            ARENA_WIDTH,
+            0.0,
+            ARENA_HEIGHT,
+        )))
+        .with(transform)
+        .build();
+}
+
+fn initialise_hunter(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
+    let mut transform = Transform::default();
+
+    // Correctly position the paddles.
+    let y = ARENA_HEIGHT / 2.0;
+    transform.set_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+
+    // Assign the sprites for the paddles
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle.clone(),
+        sprite_number: 0, // paddle is the first sprite in the sprite_sheet
+    };
+
+    // Create a hunter entity.
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Paddle::new())
+        .with(transform)
+        .build();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
-extern crate amethyst;
+mod hunter;
 
 use amethyst::{
+    core::transform::TransformBundle,
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
     utils::application_root_dir,
 };
-
-struct Example;
-
-impl SimpleState for Example {}
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
-    let path = format!(
-        "{}/resources/display_config.ron",
-        application_root_dir()
-    );
+    use crate::hunter::Hunter;
+
+    let app_root = application_root_dir();
+
+    let path = format!("{}/resources/display_config.ron", app_root);
     let config = DisplayConfig::load(&path);
 
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
             .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
+            .with_pass(DrawFlat2D::new()),
     );
+    let game_data = GameDataBuilder::default()
+        .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
+        .with_bundle(TransformBundle::new())?;
 
-    let game_data =
-        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
-    let mut game = Application::new("./", Example, game_data)?;
+    let assets_dir = format!("{}/assets/", app_root);
 
+    let mut game = Application::new(assets_dir, Hunter, game_data)?;
     game.run();
-
     Ok(())
 }
