a2bb239cfb70c87c9b983b66ad9b0700662105f4
Antoine Albertelli <antoine@antoinealb.net>
2019-01-26 16:03:30 +0100

    Fleeing & avoiding the trees

--- src/systems/behavior.rs
+++ src/systems/behavior.rs
@@ -1,12 +1,13 @@
 use amethyst::{
+    core::nalgebra::{distance, norm, normalize, Vector2},
     core::transform::Transform,
     ecs::prelude::{Join, ReadStorage, System, WriteStorage},
 };
 
-use crate::components::{VelocityCmd};
-use crate::tree::Tree;
 use crate::beast::Beast;
+use crate::components::VelocityCmd;
 use crate::hunter::Hunter;
+use crate::tree::Tree;
 
 pub struct BehaviorSystem;
 
@@ -20,9 +21,9 @@ impl<'s> System<'s> for BehaviorSystem {
         ReadStorage<'s, Hunter>,
         ReadStorage<'s, Tree>,
         ReadStorage<'s, Beast>,
-        );
+    );
 
-    fn run(&mut self, (mut commands, transforms, hunter, _trees, beasts): Self::SystemData) {
+    fn run(&mut self, (mut commands, transforms, hunter, trees, beasts): Self::SystemData) {
         //Get position of hunter
         let mut x = 0.;
         let mut y = 0.;
@@ -34,23 +35,34 @@ impl<'s> System<'s> for BehaviorSystem {
         for (cmd, trans, _beast) in (&mut commands, &transforms, &beasts).join() {
             let bx = trans.translation().x;
             let by = trans.translation().y;
-            
-            if ((bx - x) * (bx - x) + (by -y) * (by - y)).sqrt() <= DETECTION_RADIUS {
-                cmd.x = if x > bx {
-                    -PREY_SPEED
-                } else {
-                    PREY_SPEED
-                };
-                cmd.y = if y > by {
-                    -PREY_SPEED
-                } else {
-                    PREY_SPEED
-                };
-            } else {
-                cmd.x = 0.;
-                cmd.y = 0.;
+
+            let mut speed = Vector2::new(0., 0.);
+
+            // Tree avoidance based on vector fields
+            for (_, tree_transform) in (&trees, &transforms).join() {
+                let dx = bx - tree_transform.translation().x;
+                let dy = by - tree_transform.translation().y;
+
+                let mut d = Vector2::new(dx, dy);
+
+                let dst = norm(&d);
+                if (dst > 5.) {
+                    continue;
+                }
+
+                d = normalize(&d) * 1. / (dst * dst);
+                speed += d;
             }
+
+            if ((bx - x) * (bx - x) + (by - y) * (by - y)).sqrt() <= DETECTION_RADIUS {
+                speed.x += if x > bx { -PREY_SPEED } else { PREY_SPEED };
+                speed.y += if y > by { -PREY_SPEED } else { PREY_SPEED };
+            }
+
+            speed = normalize(&speed) * PREY_SPEED;
+
+            cmd.x = speed.x;
+            cmd.y = speed.y;
         }
     }
 }
-            
