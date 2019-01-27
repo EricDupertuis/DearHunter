dd53e3a5eac16ce9af56431b9f2d22d2076870cd
Antoine Albertelli <antoine@antoinealb.net>
2019-01-27 12:02:11 +0100

    More repulsion

--- src/systems/repulsive_field.rs
+++ src/systems/repulsive_field.rs
@@ -4,9 +4,9 @@ use amethyst::{
     ecs::prelude::{Join, ReadStorage, System, WriteStorage},
 };
 
-use crate::components::{Velocity};
-use crate::tree::Tree;
+use crate::components::Velocity;
 use crate::hunter::Hunter;
+use crate::tree::Tree;
 
 const D_RADIUS: f32 = 4.;
 
@@ -23,14 +23,16 @@ impl<'s> System<'s> for RepulsionSystem {
         for (_hunter, htrans, hvel) in (&hunter, &transforms, &mut velocities).join() {
             let mut speed = Vector2::new(0., 0.);
             let hunterv = Vector2::new(htrans.translation().x, htrans.translation().y);
-            
+
             for (_tree, ttrans) in (&trees, &transforms).join() {
                 let treev = Vector2::new(ttrans.translation().x, ttrans.translation().y);
                 let mut d = hunterv - treev;
                 let mut dst = d.norm();
-                if dst < 0.01 { dst = 0.01;}
+                if dst < 0.01 {
+                    dst = 0.01;
+                }
                 if dst < D_RADIUS {
-                    d = d.normalize() * 1. / (dst * dst);
+                    d = d.normalize() * 4. / (dst * dst);
                     speed += d;
                 }
             }
