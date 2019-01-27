55b7df171a476adcc16fd7d37faea8d4fd430f93
Antoine Albertelli <antoine@antoinealb.net>
2019-01-27 13:07:04 +0100

    Add lose screen

--- src/states/mod.rs
+++ src/states/mod.rs
@@ -1,9 +1,10 @@
 mod credit_state;
 mod game_state;
+mod lose_state;
 mod start_state;
 mod win_state;
 
 pub use self::{
     credit_state::CreditState, game_state::GameState, game_state::ARENA_HEIGHT,
-    game_state::ARENA_WIDTH, start_state::StartState, win_state::WinState,
+    game_state::ARENA_WIDTH, lose_state::LoseState, start_state::StartState, win_state::WinState,
 };
